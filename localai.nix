b37f36938280040a116d7dd1576b30866929bed0
Mirko Lenz <mirko@mirkolenz.com>
2023-11-10 12:34:17 +0100

    fix: add systems and metadatato localai & ollama

--- localai.nix
+++ localai.nix
@@ -1,36 +1,46 @@
 {
+  system,
   fetchurl,
   lib,
   stdenv,
   autoPatchelfHook,
-}:
-stdenv.mkDerivation rec {
-  pname = "ollama";
-  version = "1.40.0";
-  src = fetchurl {
-    url = "https://github.com/mudler/LocalAI/releases/download/v${version}/local-ai-avx512-Linux-x86_64";
-    hash = "sha256-ITdEn96zy3P9GPikOujGb4/AzsESWVOn5c9vrh+LEgY=";
+  avxVersion ? "avx512",
+}: let
+  platforms = {
+    x86_64-linux = "Linux-x86_64";
+    x86_64-darwin = "Darwin_x86_64";
   };
+  platform = platforms.${system};
+in
+  stdenv.mkDerivation rec {
+    pname = "local-ai";
+    version = "1.40.0";
+    src = fetchurl {
+      url = "https://github.com/mudler/LocalAI/releases/download/v${version}/${pname}-${avxVersion}-${platform}";
+      hash = "sha256-ITdEn96zy3P9GPikOujGb4/AzsESWVOn5c9vrh+LEgY=";
+    };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
 
-  dontUnpack = true;
-  dontBuild = true;
-  dontConfigure = true;
+    dontUnpack = true;
+    dontBuild = true;
+    dontConfigure = true;
 
-  installPhase = ''
-    runHook preInstall
-    install -m755 -D $src $out/bin/localai
-    runHook postInstall
-  '';
+    installPhase = ''
+      runHook preInstall
+      install -m755 -D $src $out/bin/local-ai
+      runHook postInstall
+    '';
 
-  meta = {
-    homepage = "https://localai.io/";
-    downloadPage = "https://github.com/mudler/LocalAI/releases";
-    description = "Get up and running with Llama 2 and other large language models locally";
-    mainProgram = "localai";
-    platforms = ["x86_64-linux"];
-  };
-}
+    meta = with lib; {
+      homepage = "https://localai.io/";
+      downloadPage = "https://github.com/mudler/LocalAI/releases";
+      description = "Get up and running with Llama 2 and other large language models locally";
+      mainProgram = "local-ai";
+      platforms = builtins.attrNames platforms;
+      license = lib.licenses.mit;
+      changelog = "https://github.com/mudler/LocalAI/releases/tag/v${version}";
+    };
+  }
--- ollama.nix
+++ ollama.nix
@@ -1,36 +1,47 @@
 {
+  system,
   fetchurl,
   lib,
   stdenv,
   autoPatchelfHook,
-}:
-stdenv.mkDerivation rec {
-  pname = "ollama";
-  version = "0.1.9";
-  src = fetchurl {
-    url = "https://github.com/jmorganca/ollama/releases/download/v${version}/ollama-linux-amd64";
-    hash = "sha256-ghEkGZN4uhKkHUCXTiTzllerbg+kNdWQznaX7aPsLAU=";
+}: let
+  platforms = {
+    x86_64-linux = "linux-amd64";
+    aarch64-linux = "linux-arm64";
+    x86_64-darwin = "darwin";
+    aarch64-darwin = "darwin";
   };
+  platform = platforms.${system};
+in
+  stdenv.mkDerivation rec {
+    pname = "ollama";
+    version = "0.1.9";
+    src = fetchurl {
+      url = "https://github.com/jmorganca/ollama/releases/download/v${version}/${pname}-${platform}";
+      hash = "sha256-ghEkGZN4uhKkHUCXTiTzllerbg+kNdWQznaX7aPsLAU=";
+    };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
 
-  dontUnpack = true;
-  dontBuild = true;
-  dontConfigure = true;
+    dontUnpack = true;
+    dontBuild = true;
+    dontConfigure = true;
 
-  installPhase = ''
-    runHook preInstall
-    install -m755 -D $src $out/bin/ollama
-    runHook postInstall
-  '';
+    installPhase = ''
+      runHook preInstall
+      install -m755 -D $src $out/bin/ollama
+      runHook postInstall
+    '';
 
-  meta = {
-    homepage = "https://ollama.ai/";
-    downloadPage = "https://github.com/jmorganca/ollama/releases";
-    description = "Get up and running with Llama 2 and other large language models locally";
-    mainProgram = "ollama";
-    platforms = ["x86_64-linux"];
-  };
-}
+    meta = {
+      homepage = "https://ollama.ai/";
+      downloadPage = "https://github.com/jmorganca/ollama/releases";
+      description = "Get up and running with Llama 2 and other large language models locally";
+      mainProgram = "ollama";
+      platforms = builtins.attrNames platforms;
+      license = lib.licenses.mit;
+      changelog = "https://github.com/jmorganca/ollama/releases/tag/v${version}";
+    };
+  }
