c3ef71cc08b0a35312634668b6b304c6946755ef
Mirko Lenz <mirko@mirkolenz.com>
2023-11-15 23:08:39 +0100

    feat: add (non-functional) llama-cpp-python pkg

--- flake.nix
+++ flake.nix
@@ -3,12 +3,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    llama-cpp = {
+      url = "github:ggerganov/llama.cpp";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs @ {
     self,
     nixpkgs,
     flake-parts,
     systems,
+    llama-cpp,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -37,6 +42,15 @@
           local-ai = pkgs.callPackage ./local-ai.nix {};
           localai = self'.packages.local-ai;
           litellm = pkgs.callPackage ./litellm.nix {};
+          llama-cpp = pkgs.callPackage ./llama-cpp-python.nix {
+            llama-cpp = llama-cpp.packages.${system}.default;
+          };
+          llama-cpp-cuda = pkgs.callPackage ./llama-cpp-python.nix {
+            llama-cpp = llama-cpp.packages.${system}.cuda;
+          };
+          llama-cpp-opencl = pkgs.callPackage ./llama-cpp-python.nix {
+            llama-cpp = llama-cpp.packages.${system}.opencl;
+          };
         };
       };
     };
