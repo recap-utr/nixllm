638701b766778175a55ca1ecd17c6783c0957c01
Mirko Lenz <mirko@mirkolenz.com>
2023-11-30 21:05:22 +0100

    chore(deps): bump ollama to v0.1.13

--- ollama.nix
+++ ollama.nix
@@ -6,20 +6,20 @@
 }: let
   inherit (stdenv.hostPlatform) system;
   pname = "ollama";
-  version = "0.1.12";
+  version = "0.1.13";
   repo = "https://github.com/jmorganca/ollama";
   srcs = rec {
     x86_64-linux = {
       url = "${repo}/releases/download/v${version}/${pname}-linux-amd64";
-      hash = "sha256-qJDWbDdiGu1THcd5X6YrKsG4vrzOh/VsXO81sCajrQ8=";
+      hash = "sha256-tsSjTSC5M2haM28gcGeDIInSD7dGf83lhbiSBYd5YZg=";
     };
     aarch64-linux = {
       url = "${repo}/releases/download/v${version}/${pname}-linux-arm64";
-      hash = "sha256-g0eydRJZQdjR8+hf0migeMoNQg+syLqPsQBGUdZ5UoQ=";
+      hash = "sha256-bfnVvhnhCsI73aJAgsFeXRWnlumwT0rdROMmtYcRfD4=";
     };
     x86_64-darwin = {
       url = "${repo}/releases/download/v${version}/${pname}-darwin";
-      hash = "sha256-JjOznbRgnzpH3i+ElFTuJFlzpaw7LnJyIe+Gt2AoZeI=";
+      hash = "sha256-AEeG3TaOi8y+kO1JTunx6sKfq/f4nL5ULScoD6xP2Tk=";
     };
     aarch64-darwin = x86_64-darwin;
   };
